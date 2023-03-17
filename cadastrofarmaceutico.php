d2a55b16a9731f9630acce811cce35312b720c17
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-17 13:14:15 -0300

    Alutalizando cadastrar e cadastro farmaceutico

--- cadastrofarmaceutico.php
+++ cadastrofarmaceutico.php
@@ -1,21 +1,5 @@
-<?php
-//session_start();
-if(isset($_POST['submit'])){
 
-    $nome = ($_POST['nome']);
-    $senha = ($_POST['senha']);
-    $numero = $_POST['numero'];
-
-    //inserindo dados no banco para cadastro
-    $result = mysqli_query($conexao,
-    "INSERT INTO adastrousuário(nome,senha,numero)
-     VALUES ('$nome','$senha','$numero')  ");
-
-}
-
-?><
-
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
