69afe136f6265ab578da2e2fd830528ef87028f3
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-17 11:43:00 -0300

    Inserindo dados do formularios no banco de dados

--- cadastrofarmaceutico.php
+++ cadastrofarmaceutico.php
@@ -1,5 +1,21 @@
+<?php
+//session_start();
+if(isset($_POST['submit'])){
+
+    $nome = ($_POST['nome']);
+    $senha = ($_POST['senha']);
+    $numero = $_POST['numero'];
+
+    //inserindo dados no banco para cadastro
+    $result = mysqli_query($conexao,
+    "INSERT INTO cadastrousuário(nome,senha,numero)
+     VALUES ('$nome','$senha','$numero')    ");
+
+}
+
+?>
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
