70aba4928f09c5c22d6d253c7c443fe325224efb
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-17 14:31:58 -0300

    atualizar estrutura tabela

--- visualizar.php
+++ visualizar.php
@@ -1,24 +1,30 @@
 <?php
     
     include_once 'conexao.php';
+
     $result = "SELECT * FROM  cadastrousuário";
     $resulta_dodados = mysqli_query($conexao, $result);
     $tabela = '<table border="1">';
 
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Listagem de cadastro</title>
+</head>
 <body>
+    <header class="headerctelacadastro">
 
-<header class="headerctelacadastro">
+        <h1 id="h1cadastro"> Cadastro de Medicamentos </h1>
 
-    <h1 id="h1cadastro"> Cadastro de Farmaceutico </h1>
+    </header>
 
-</header>
-
-<div class="Divtelainicial" >
-    
-    <h1> Farmaceutico cadastrados </h1>
-    
-    <table border="1">
+    <div class="Divtelainicial" >
+        <table border="1">
                 <thead>
                     <tr>
                         <th> ID </th>
@@ -34,8 +40,8 @@
                    
                         $id = $row_usuario ['id']. "<br>"; 
                         $nome= $row_usuario['nome'] . "<br>";
-                        $celular= $row_usuario['senha']. "<br>";
-                        $cidade= $row_usuario['numero']. "<br>";
+                        $senha= $row_usuario['senha']. "<br>";
+                        $numero= $row_usuario['numero']. "<br>";
                      
                     ?>  
                     <tr>
@@ -50,12 +56,11 @@
                     ?>
 
                 </tbody>
-        </table>
+         </table>
+         </div>
+    <footer>
+        <p> BJJSoftware2023</p>
+    </footer>             
     
-
-</div>
-<footer>
-    <p> BJJSoftware2023 </p>
-</footer>
-
 </body>
+</html>
