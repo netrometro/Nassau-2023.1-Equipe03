3bcf4f6edc2d4690b5954f3e27cf37f48cf830af
Juely2021 <juely.menezes@gmail.com>
2023-03-18 00:17:28 -0300

    exclusão de php com falha

---
+++ cadastro.php
@@ -0,0 +1,18 @@
+<?php
+//session_start();
+include_once 'conexao.php';
+
+if(isset($_POST['submit'])){
+
+    $nome = ($_POST['nome']);
+    $senha = ($_POST['senha']);
+    $numero = $_POST['numero'];
+
+    //inserindo dados no banco para cadastro
+    $result = mysqli_query($conexao,
+    "INSERT INTO cadastrousuário(nome,senha,numero)
+     VALUES ('$nome','$senha','$numero')  ");
+
+}
+
+?>
