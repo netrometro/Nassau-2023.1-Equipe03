7952595c7c8e2d7a576e335ba254097c04e27cc1
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-24 12:24:53 -0300

    Criando o arquivo de alterar dadosdo banco de dados

--- excluir.php
+++ excluir.php
@@ -4,7 +4,8 @@ include_once 'conexao.php';
     if(isset($_POST['submit'])){
         $id = $_POST['id'];
         //Deletando dados no banco para cadastro
-        $result = "DELETE * FROM  cadastrousuário WHERE id= '$id' "; 
-        $resulta_dodados = mysqli_query($conexao, $result);
+        $result = "DELETE FROM  cadastromedicamentos WHERE id= '$id' "; 
+        $resulta_dados = mysqli_query($conexao, $result) or die('Erro ao deletar');
+        echo"deletado";
     }
 ?>
