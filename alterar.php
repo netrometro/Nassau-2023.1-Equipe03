bee34b9abec2a71ce4b355719df45e031e2de137
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-24 12:26:26 -0300

    inserindo condicoção de id para alterar dados

---
+++ alterar.php
@@ -0,0 +1,11 @@
+<?php
+//session_start();
+include_once 'conexao.php';
+    if(isset($_POST['submit'])){
+        $id = $_POST['id'];
+        //Deletando dados no banco para cadastro
+        $result = "DELETE FROM  cadastromedicamentos WHERE id= '$id' "; 
+        $resulta_dados = mysqli_query($conexao, $result) or die('Erro ao deletar');
+        echo"deletado";
+    }
+?>
