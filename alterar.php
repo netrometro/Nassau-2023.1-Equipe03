7e6b0f7ee2b1201821a0344502ec1531c34ff98a
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-24 13:01:46 -0300

    configurando o update

--- alterar.php
+++ alterar.php
@@ -3,9 +3,17 @@
 include_once 'conexao.php';
     if(isset($_POST['submit'])){
         $id = $_POST['id'];
-        //Deletando dados no banco para cadastro
-        $result = "UPDATE cadastromedicamentos SET codigo ='$codigo', validade = '$data', fabricante='$fabricante' WHERE id= '$id' "; 
-        $resulta_dados = mysqli_query($conexao, $result) or die('Erro ao alterar');
-        echo"ALteração feita com sucesso";
+        
+        //Alterando dados no banco para cadastro
+        if(isset($_POST['codigo'])  && (isset($_POST['datan'])) (isset($_POST['fabricante']))){
+
+            $result = "UPDATE cadastromedicamentos SET codigo ='$codigo', validade = '$data', fabricante='$fabricante' WHERE id= '$id' "; 
+            $resulta_dados = mysqli_query($conexao, $result) or die('Erro ao alterar');
+            echo"ALteração feita com sucesso";
+
+        }
+       
+
+
     }
 ?>
