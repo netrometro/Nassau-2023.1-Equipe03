1f9eba0d78f3cb6f0d887a90d04b9d57c264a36b
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-24 12:54:16 -0300

    conclusão de inserção do update para  alerar dados das tabela

--- alterar.php
+++ alterar.php
@@ -4,8 +4,8 @@ include_once 'conexao.php';
     if(isset($_POST['submit'])){
         $id = $_POST['id'];
         //Deletando dados no banco para cadastro
-        $result = "DELETE FROM  cadastromedicamentos WHERE id= '$id' "; 
-        $resulta_dados = mysqli_query($conexao, $result) or die('Erro ao deletar');
-        echo"deletado";
+        $result = "UPDATE cadastromedicamentos SET codigo ='$codigo', validade = '$data', fabricante='$fabricante' WHERE id= '$id' "; 
+        $resulta_dados = mysqli_query($conexao, $result) or die('Erro ao alterar');
+        echo"ALteração feita com sucesso";
     }
 ?>
