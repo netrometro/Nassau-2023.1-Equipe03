fef9f6e978803b084704f0c3391241194b0b111c
Juely2021 <juely.menezes@gmail.com>
2023-03-31 20:57:23 -0300

    adicionado while

--- exibirClientes.php
+++ exibirClientes.php
@@ -6,7 +6,22 @@ $dbname ="db_projeto";
 
 //Criar a conexão
 $conexao = mysqli_connect ($servername, $usurname, $password, $dbname);
+
+//exibindo dados de medicamentos e usuários
+$result = "SELECT * FROM db_projeto";
+$resulta_dados = mysqli_query($conexao, $result);
+$tabela = '<table border="1">';
+
+while($row_usuario = mysqli_fetch_assoc($resulta_dados)){
+
+                $id = $row_clientes['id']. "<br>";
+                $codigo= $row_clientes['nome']. "<br>";
+                $data = $row_clientes['email']. "<br>";
+                $fabricante = $row_clientes['fabricante']. "<br>";
+}
+
 ?>
+
 
 <table border="1" width="100%">
     <tr>
