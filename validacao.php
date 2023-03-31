3deb7a78ca0c9e554c35cd85a79a2e559b5a560d
Juely2021 <juely.menezes@gmail.com>
2023-03-31 19:45:59 -0300

    validacao de usuario

--- validacao.php
+++ validacao.php
@@ -1,10 +1,5 @@
 <?php
 
-  // Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
-//   if (!empty($_POST) AND (empty($_POST['usuario']) OR empty($_POST['senha']))) {
-//       header("Location: index.php"); exit;
-//   }
-
 // Tenta se conectar ao servidor MySQL
 $servidor = "localhost";
 $usuario = "root";
@@ -12,30 +7,19 @@ $senha = "ju1@";
 $dbname = "db_projeto";
 
 //Criar a conexão
-$con = mysqli_connect ($servidor, $usuario, $senha, $dbname);
-$usuario_email = $_POST['email'];
-$usuario_senha = $_POST['senha'];
+$con = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-$sql="SELECT * FROM usuario WHERE TRIM(email) = $usuario_email";
-$result=mysqli_query($con,$sql);
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+    echo "Passou";
+}
+
+$usuario_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$usuario_email = mysqli_real_escape_string($con, $usuario_email);
+$usuario_senha = mysqli_real_escape_string($con, $_POST['senha']);
+
+$sql = "SELECT * FROM usuario WHERE TRIM(email) = '$usuario_email'";
+$result = mysqli_query($con, $sql);
 var_dump($result);
 echo $usuario_email;
-
-// if ())
-// {
-// // Return the number of rows in result set
-// $rowcount=mysqli_num_rows($result);
-// printf("Result set has %d rows.\n",$rowcount);
-// // Free result set
-// //mysqli_free_result($result);
-// }
-
-//   if (mysqli_num_rows($query) === 0) {
-//       // Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
-//       echo "Login inválido!";
-//   } else {
-//       // Salva os dados encontados na variável $resultado
-//       $resultado = mysqli_fetch_assoc($query);
-//   }
-
-?>
