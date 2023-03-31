c26a93de398209e505cf04ffebd6669b58b2b046
jenniferlimabarros <jennifercristianelima@gmail.com>
2023-03-31 20:44:43 -0300

    concluindo a historia

--- editamedicamentos.php
+++ editamedicamentos.php
@@ -1,5 +1,5 @@
 <?php
- include_once 'conexao.php';
+    include_once 'conexao.php';
 ?>
 <?php
 
@@ -10,6 +10,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,60 +18,59 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <title> Medicamentos </title>
 </head>
+
 <body>
 
-    <div class ="Divtelainicial">
+    <div class="Divtelainicial">
 
-        <h1>  Aletrar dados Medicamentos </h1>
+        <h1> Aletrar dados Medicamentos </h1>
 
-        <form action = "alterar.php" method="POST">
+        <form action="alterar.php" method="POST">
 
-            <input type= "numero" name ="id" placeholder="Digite o id para alterar os"><br>
-            <input type= "numero" name ="codigo" placeholder="Digite o novo codigo"><br>
-            <input type= "date" name ="datan" placeholder="Digite a nova data"><br>
-            <input type= "fabricante" name ="fabricante" placeholder="Digite o novo fabricante"><br>
+            <input type="numero" name="id" placeholder="Digite o id para alterar os"><br>
+            <input type="numero" name="codigo" placeholder="Digite o novo codigo"><br>
+            <input type="date" name="datan" placeholder="Digite a nova data"><br>
+            <input type="fabricante" name="fabricante" placeholder="Digite o novo fabricante"><br>
 
-            <input id="cadastrados"  type="submit" name ="submit" value="Alterar">
+            <input id="cadastrados" type="submit" name="submit" value="Alterar">
 
         </form>
         <br><br>
 
-    <table border="1" width="100%"> 
+        <table border="1" width="100%">
 
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>CÓDIGO</th>
-            <th>DATA VALIDADE </th>
-            <th>FABRICANTE</th>
-        </tr>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>CÓDIGO</th>
+                    <th>DATA VALIDADE </th>
+                    <th>FABRICANTE</th>
+                </tr>
 
-    </thead>
-    <tbody>
-        <?php
+            </thead>
+            <tbody>
+                <?php
 
-            while($row_usuario = mysqli_fetch_assoc($resulta_dados)){
+                while ($row_usuario = mysqli_fetch_assoc($resulta_dados)) {
 
-                $id = $row_usuario['id']. "<br>";
-                $codigo= $row_usuario['codigo']. "<br>";
-                $data = $row_usuario['validade']. "<br>";
-                $fabricante = $row_usuario['fabricante']. "<br>";
+                    $id = $row_usuario['id'] . "<br>";
+                    $codigo = $row_usuario['codigo'] . "<br>";
+                    $data = $row_usuario['validade'] . "<br>";
+                    $fabricante = $row_usuario['fabricante'] . "<br>";
 
-        ?>    
-        <tr>
-            <td> <?php echo $id?> </td>
-            <td> <?php echo $codigo?> </td>
-            <td> <?php echo $data?> </td>
-            <td> <?php echo $fabricante?> </td>
-        </tr> <br>
-        <?php
-            }
-        ?>
-    </tbody>
-</table>
-
-
+                ?>
+                    <tr>
+                        <td> <?php echo $id ?> </td>
+                        <td> <?php echo $codigo ?> </td>
+                        <td> <?php echo $data ?> </td>
+                        <td> <?php echo $fabricante ?> </td>
+                    </tr> <br>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
-    
 </body>
+
 </html>
