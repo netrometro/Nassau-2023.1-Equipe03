844aba1362270d0daa2f82f35980664ebbb240f9
Juely2021 <juely.menezes@gmail.com>
2023-03-31 20:34:02 -0300

    criado conexão com o banco

--- exibirClientes.php
+++ exibirClientes.php
@@ -1,3 +1,13 @@
+<?php
+$servername = "localhost";
+$usurname = "root";
+$password = "ju1@";
+$dbname ="db_projeto";
+
+//Criar a conexão
+$conexao = mysqli_connect ($servername, $usurname, $password, $dbname);
+?>
+
 <table border="1" width="100%">
     <tr>
         <td>Id</td>
