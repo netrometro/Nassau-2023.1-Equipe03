49b299e3bae6f612277e89110eb6c4c31d980cde
brenno16 <brenno1413@hotmail.com>
2023-03-31 21:22:01 -0300

    crud completo da tabela de medicamentos

--- cadastraMedicamentos.php
+++ cadastraMedicamentos.php
@@ -1,48 +1,64 @@
-<?php
-    session_start();
-?>
-
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Cadastrar medicamentos</h1>
-    <?php
-        if(isset($_SESSION['msg'])) {
-            echo $_SESSION['msg'];
-            unset($_SESSION['msg']);
-        }
-    ?>
-    <!-- DADOS CADASTRO MEDICAMENTOS -->
-    <form action="processa.php" method="post">
-        <input type="text" name="codigo" placeholder="Digite o codigo do medicamento">
-        <input type="text" name="fornecedor" placeholder="Informe fornecedor">
-        <input type="text" name="fabricante" placeholder="informe fabricante">
-        <input type="text" name="preco" placeholder="Valor do Medicamento">
-        <input type="submit" value="Cadastrar">
-    </form>
+    <div>
+        <h2>lista</h2>
+        <a href="create.php">Novo Cadastro</a>
+        <br>
+        <table>
+            <thead>
+                <tr>
+                    <th>codigp</th>
+                    <th>fornecedor</th>
+                    <th>fabricante</th>
+                    <th>preço</th>
+                    <th>E</th>
+                    <td>D</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $servidor = "localhost";
+                $usuario = "root";
+                $senha = "";
+                $dbname = "db_projeto";
+                
+                //Criar a conexão
+                $con = mysqli_connect ($servidor, $usuario, $senha, $dbname);
 
-    <?php
-        
-        $conn = mysqli_connect("localhost","root","", "db_projeto");
-        $resultado = mysqli_query($conn,"SELECT * FROM medicamentos");
-        echo "<table>";
-        echo "<tr><th>codigo</th><th>fornecedor</th><th>fabricante</th><th>preco</th></tr>";
-        while ($linha = mysqli_fetch_assoc($resultado)) {
-        echo "<tr>";
-        echo "<td>" . $linha['codigo'] . "</td>";
-        echo "<td>" . $linha['fornecedor'] . "</td>";
-        echo "<td>" . $linha['fabricante'] . "</td>";
-        echo "<td>" . $linha['preco'] . "</td>";
-        echo "</tr>";
-}
-echo "</table>";
+                if($con->connect_error) {
+                    die("falha: " . $con->connect_error);
+                }
+                $sql= "SELECT * FROM medicamentos";
+                $result = $con->query($sql);
 
-   ?>
+                if(!$result){
+                    die("Invalido " . $con->connect_error);
+                }
+
+                while($row = $result->fetch_assoc()){
+                    echo "
+                    <tr>
+                    <td>$row[codigo]</td>
+                    <td>$row[fornecedor]</td>
+                    <td>$row[fabricante]</td>
+                    <td>$row[preco]</td>
+                    <td>
+                        <a href='/NASSAU-2023.1-EQUIPE03/edit.php?codigo=$row[codigo]'>edit</a>
+                        <a href='/NASSAU-2023.1-EQUIPE03/delete.php?codigo=$row[codigo]'>delete</a>
+                    </td>
+                </tr>
+                    ";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
